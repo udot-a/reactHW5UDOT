ec76923a7757c0f29d85f7b432ab2725df24fdb1
Udot Andrii <udot.dron@gmail.com>
2019-12-01 21:17:51 +0200

    'someTrubble'

--- src/Container.jsx
+++ src/Container.jsx
@@ -4,7 +4,7 @@ import Item from "./Item";
 export default (props) => {
 
         const list = props.array.map(item => {
-            return <Item item={item.content} />
+            return <Item item={item.content} key={item.key} />
         })
         return <ul>{list}</ul>
 }
