2d2a4c321f115943922df83b09cf5ba1791745c0
Udot Andrii <udot.dron@gmail.com>
2019-12-01 19:38:17 +0200

    Updates

---
+++ precache-manifest.e20b6a8eb4cf51d7130d31d29a366f62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bd7fd46ac2d48e082f946ac7f011b1f",
+    "url": "/reactHW5UDOT/index.html"
+  },
+  {
+    "revision": "f997cdfe98b02c2ed517",
+    "url": "/reactHW5UDOT/static/css/main.0af3fdce.chunk.css"
+  },
+  {
+    "revision": "4c3378898583264a87f0",
+    "url": "/reactHW5UDOT/static/js/2.939f798a.chunk.js"
+  },
+  {
+    "revision": "f997cdfe98b02c2ed517",
+    "url": "/reactHW5UDOT/static/js/main.f9d837f9.chunk.js"
+  },
+  {
+    "revision": "c292c506a98fbedd1c42",
+    "url": "/reactHW5UDOT/static/js/runtime-main.6e404a97.js"
+  }
+]);
